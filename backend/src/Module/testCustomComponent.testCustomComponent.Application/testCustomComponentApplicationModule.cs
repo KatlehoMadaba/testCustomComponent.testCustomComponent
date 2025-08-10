b48de46672d3d00784d355acb239abe1048b0229
KatlehoMadaba <101579604+KatlehoMadaba@users.noreply.github.com>
2025-08-10 22:55:24 +0200

    first commit

---
+++ backend/src/Module/testCustomComponent.testCustomComponent.Application/testCustomComponentApplicationModule.cs
@@ -0,0 +1,90 @@
+using System.Reflection;
+using System.Threading.Tasks;
+using Abp.AspNetCore;
+using Abp.AspNetCore.Configuration;
+using Abp.AutoMapper;
+using Abp.Modules;
+using Abp.Reflection.Extensions;
+using Intent.RoslynWeaver.Attributes;
+using testCustomComponent.testCustomComponent.Domain;
+using Shesha;
+using Shesha.Modules;
+using Shesha.Startup;
+using Shesha.Web.FormsDesigner;
+
+[assembly: IntentTemplate("Boxfusion.Modules.Application.Services.AppService", Version = "1.0")]
+[assembly: DefaultIntentManaged(Mode.Fully)]
+
+namespace testCustomComponent.testCustomComponent.Application
+{
+    [IntentManaged(Mode.Ignore)]
+    /// <summary>
+    /// testCustomComponent Module
+    /// </summary>
+    [DependsOn(
+        typeof(testCustomComponentModule),
+        typeof(SheshaCoreModule),
+        typeof(AbpAspNetCoreModule)
+    )]
+    public class testCustomComponentApplicationModule : SheshaSubModule<testCustomComponentModule>
+    {
+        public override async Task<bool> InitializeConfigurationAsync()
+        {
+            // Import any configuration embeded as resources in this assembly on application start-up.
+            return await ImportConfigurationAsync();
+        }
+
+        /// inheritedDoc
+        public override void Initialize()
+        {
+            var thisAssembly = Assembly.GetExecutingAssembly();
+            IocManager.RegisterAssemblyByConvention(thisAssembly);
+
+            Configuration.Modules.AbpAutoMapper().Configurators.Add(
+                // Scan the assembly for classes which inherit from AutoMapper.Profile
+                cfg => cfg.AddMaps(thisAssembly)
+            );
+        }
+
+        /// inheritedDoc
+        public override void PreInitialize()
+        {
+            base.PreInitialize();
+
+            Configuration.Modules.AbpAspNetCore()
+                .CreateControllersForAppServices(
+                    typeof(SheshaCoreModule).GetAssembly()
+                );
+
+            Configuration.Modules.AbpAspNetCore()
+                 .CreateControllersForAppServices(
+                     typeof(SheshaApplicationModule).GetAssembly()
+                 );
+
+            Configuration.Modules.AbpAspNetCore()
+                 .CreateControllersForAppServices(
+                     typeof(SheshaFormsDesignerModule).GetAssembly()
+                 );
+
+            Configuration.Modules.AbpAspNetCore()
+                 .CreateControllersForAppServices(
+                     typeof(SheshaFrameworkModule).GetAssembly()
+                 );
+
+            Configuration.Modules.AbpAspNetCore().CreateControllersForAppServices(
+               typeof(testCustomComponentApplicationModule).Assembly,
+               moduleName: "testCustomComponent",
+                useConventionalHttpVerbs: true);
+
+            Configuration.Modules.AbpAspNetCore()
+                 .CreateControllersForAppServices(
+                     typeof(testCustomComponentModule).GetAssembly()
+                 );
+
+            Configuration.Modules.AbpAspNetCore()
+                 .CreateControllersForAppServices(
+                     typeof(testCustomComponentApplicationModule).GetAssembly()
+                 );
+        }
+    }
+}
